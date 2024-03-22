7bd9620d722adda5acda031a02816b5a9af07df2
MarcElosua <elosua.marc@gmail.com>
2024-03-22 12:04:55 -0400

    day2 material gtg

---
+++ navigating-the-single-cell-landscape-a-comprehensive-workshop-on-scrnaseq/day-2/render_notebooks.sh
@@ -0,0 +1,4 @@
+R -e 'quarto::quarto_render("6-CCC-analysis.qmd", output_format = "html")'
+R -e 'quarto::quarto_render("7-factor-analysis.qmd", output_format = "html")'
+R -e 'quarto::quarto_render("8-gene-signatures.qmd", output_format = "html")'
+R -e 'quarto::quarto_render("9-activity-scoring.qmd", output_format = "html")'
